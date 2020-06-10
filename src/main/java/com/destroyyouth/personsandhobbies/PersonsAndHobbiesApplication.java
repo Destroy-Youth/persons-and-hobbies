2d4639f578c0bf8f4460c11173ce8b1b4d084378
Equihua <luis.herrerae@outlook.com>
2020-06-10 18:40:22 -0500

    First commit

---
+++ src/main/java/com/destroyyouth/personsandhobbies/PersonsAndHobbiesApplication.java
@@ -0,0 +1,13 @@
+package com.destroyyouth.personsandhobbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonsAndHobbiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonsAndHobbiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/destroyyouth/personsandhobbies/PersonsAndHobbiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destroyyouth.personsandhobbies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonsAndHobbiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
