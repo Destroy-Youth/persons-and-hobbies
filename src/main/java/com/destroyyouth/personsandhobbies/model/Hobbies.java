776427a48cc4fde2ba9f77c10a974e9d0cb05afc
Equihua <luis.herrerae@outlook.com>
2020-06-10 20:19:39 -0500

    Entities added

---
+++ src/main/java/com/destroyyouth/personsandhobbies/model/Hobbies.java
@@ -0,0 +1,42 @@
+package com.destroyyouth.personsandhobbies.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Hobbies
+ */
+@Entity
+public class Hobbies implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 4819239186181304922L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
