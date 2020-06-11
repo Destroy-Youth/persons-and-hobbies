6dfa00e6eaf6032b748a08422f289c2d0b89109d
Equihua <luis.herrerae@outlook.com>
2020-06-10 20:19:20 -0500

    Change to derby instead 👀

--- src/main/java/com/destroyyouth/personsandhobbies/PersonsAndHobbiesApplication.java
+++ src/main/java/com/destroyyouth/personsandhobbies/PersonsAndHobbiesApplication.java
@@ -1,32 +1,13 @@
 package com.destroyyouth.personsandhobbies;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class PersonsAndHobbiesApplication {
 
-	@Autowired
-	Environment env;
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonsAndHobbiesApplication.class, args);
-	}
-
-	@Bean
-	public DataSource dataSource() {
-		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(env.getProperty("driverClassName"));
-		dataSource.setUrl(env.getProperty("url"));
-		dataSource.setUsername(env.getProperty("user"));
-		dataSource.setPassword(env.getProperty("password"));
-		return dataSource;
 	}
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO persons (id, first_name, last_name, mail, sex, id_hobbie) VALUES
+  (1, 'Wes', 'Bos', 'wes.bos@mail.com', 'm', 1);
+
+INSERT INTO hobbies (id, name) VALUES
+  (1, 'Coding');
